b8aea0e20a4fdb574ef830c561d39b3eb94d2d4d
odm275 <pomejia@gmail.com>
2020-10-06 20:24:16 -0500

    front page

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { Layout } from "antd";
+import { HomeHero } from "./components";
 
-export const Home = () => {
-  return <h2>Home</h2>;
+import mapBackground from "./assets/map-background.jpg";
+
+const { Content } = Layout;
+
+export const Home = ({ history }: RouteComponentProps) => {
+  const onSearch = (value: string) => {
+    history.push(`/listings/${value}`);
+  };
+  return (
+    <Content
+      className="home"
+      style={{ backgroundImage: `url(${mapBackground})` }}
+    >
+      <HomeHero onSearch={onSearch} />
+    </Content>
+  );
 };
